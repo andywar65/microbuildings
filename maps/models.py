6783bae29a074290fba0a9d3c7c05f40acd68bfd
andywar65 <andy.war1965@gmail.com>
2022-01-03 18:40:57 +0100

    added two models

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField("Category name",
+        max_length = 50, )
+    visible = models.BooleanField("Is category immediately visible", default=True)
+
+    def __str__(self):
+        return self.name
+
+class Building(models.Model):
+    name = models.CharField("Building name",
+        max_length = 50, )
+    location = models.PointField( srid=4326, geography=True )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+        verbose_name = "Building's category")
+
+    def __str__(self):
+        return self.name
